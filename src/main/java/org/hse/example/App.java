3aca7f5bdaa5b5f212537d5a8894ff5a3e272254
vrusakov <rusakov_vas@mail.ru>
2020-09-29 22:26:23 +0500

    Объектно-ориентированная реализация. Реализация в стиле Java 8

--- src/main/java/org/hse/example/App.java
+++ src/main/java/org/hse/example/App.java
@@ -1,9 +1,14 @@
 package org.hse.example;
 
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 /**
  * Приложение для работы со счастливым
  */
 public class App {
+
+    private static long count = 0; // для грязного хака с лямблами
 
     /**
      * Основной метод. Вычисляет количество счастливых билетов
@@ -11,15 +16,41 @@ public class App {
      * @param args аргументы
      */
     public static void main(String... args) {
-        MealTicketCounter mealTicketCounter = new MealTicketCounter();
+        Consumer<Ticket> mealTicketCounter = getTicketConsumer();
         for (long i = 0; i < 1000000; i++) {
             //Получаем билет
             Ticket ticket = new Ticket(i);
 
             //Обрабтываем полученный билет
-            mealTicketCounter.process(ticket);
+            mealTicketCounter.accept(ticket) ;
         }
-        System.out.println("Счастливых билетов " + mealTicketCounter.getCount());
+        System.out.println("Счастливых билетов " + count);
+    }
+
+    /**
+     * Возвращает {@link Consumer} экземпляров класса {@link Ticket}.
+     * В данном случае обработка следующая: считает количество счастливых билетов
+     * сохраняя результат в {@link App#count} и выводит в консоль счастливые билеты
+     *
+     * @return сформированный
+     */
+    private static Consumer<Ticket> getTicketConsumer() {
+        Consumer<Ticket> mealTicketCounter =
+                (ticket) -> {
+                    if (!ticket.isMealTicket()) {
+                        return;
+                    }
+
+                    count++;
+                };
+        return mealTicketCounter
+                .andThen((ticket) -> {
+                    if (!ticket.isMealTicket()) {
+                        return;
+                    }
+
+                    System.out.println(ticket.toString());
+                });
     }
 
 }
